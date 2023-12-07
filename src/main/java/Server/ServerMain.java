dea9ac92b1060d11eab9cb24dec9c4c39cc571b4
Martin325226 <147009253+Martin325226@users.noreply.github.com>
2023-12-07 13:04:08 -0500

    Update ServerMain.java

--- src/main/java/Server/ServerMain.java
+++ src/main/java/Server/ServerMain.java
@@ -1,2 +1,19 @@
-package Server;public class ServerMain {
+package Server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import java.io.IOException;
+
+public class ServerMain {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        int port = 50051; // Default port
+        Server server = ServerBuilder.forPort(port)
+                .addService(new ForumServiceImpl())
+                .build();
+
+        System.out.println("Starting server...");
+        server.start();
+        System.out.println("Server started on port " + port);
+        server.awaitTermination();
+    }
 }
