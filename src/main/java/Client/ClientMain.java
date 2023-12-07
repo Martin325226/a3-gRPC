626df4680e3a7a23af4ee7b6f39b7e04b849d81d
Martin325226 <147009253+Martin325226@users.noreply.github.com>
2023-12-07 13:05:14 -0500

    Update ClientMain.java

--- src/main/java/Client/ClientMain.java
+++ src/main/java/Client/ClientMain.java
@@ -1,2 +1,36 @@
-package Client;public class ClientMain {
+package Client;
+
+public class ClientMain {
+    public static void main(String[] args) {
+        ForumClient client = new ForumClient("localhost", 50051);
+
+        // Create a new post
+        String newPostTitle = "Hello gRPC";
+        String newPostText = "This is a sample post";
+        String author = "User123";
+        client.createPost(newPostTitle, newPostText, author);
+
+        // Assuming the new post has an ID of "post1"
+        String postId = "post1";
+
+        // Vote on the post
+        client.votePost(postId, true); // Upvote the post
+
+        // Retrieve the post
+        client.getPost(postId);
+
+        // Create a comment on the post
+        String commentText = "This is a sample comment";
+        client.createComment(postId, author, commentText);
+
+        // Assuming the new comment has an ID of "comment1"
+        String commentId = "comment1";
+
+        // Vote on the comment
+        client.voteComment(commentId, true); // Upvote the comment
+
+        // Retrieve the top N comments under a post
+        int topN = 5;
+        client.getTopComments(postId, topN);
+    }
 }
